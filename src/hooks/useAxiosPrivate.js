b2f99599fa88e676f38eda9df7f7e24b2035c6f0
1701manpt <thaiphuongnam1071@gmail.com>
2022-12-24 15:10:47 +0700

    xong refresh token hoàn hảo

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -8,44 +8,44 @@ const useAxiosPrivate = () => {
     const dispatch = useDispatch()
     const user = useSelector((state) => state.auth.login.currentUser)
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        const requestInterCept = axiosPrivate.interceptors.request.use(
-            config => {
-                if (!config.headers['token']) {
-                    config.headers['token'] = `Bearer ${user.token}`
-                }
-
-                return config
-            },
-            async (error) => Promise.reject(error)
-        )
-
-        const responseIntercept = axiosPrivate.interceptors.response.use(
-            response => {
-                return response
-            },
-            async (error) => {
-                const prevRequest = error?.config
-                if (error?.response?.status === 401 && !prevRequest?.sent) {
-                    prevRequest.sent = true
-
-                    dispatch(refreshToken())
-
-                    prevRequest.headers['token'] = `Bearer ${user.token}`
-
-                    return axiosPrivate(prevRequest)
-                }
-
-                return Promise.reject(error)
+    const requestInterCept = axiosPrivate.interceptors.request.use(
+        config => {
+            if (!config.headers['token']) {
+                config.headers['token'] = `Bearer ${user.token}`
             }
-        )
 
-        return () => {
-            axiosPrivate.interceptors.request.eject(requestInterCept)
-            axiosPrivate.interceptors.response.eject(responseIntercept)
+            return config
+        },
+        async (error) => Promise.reject(error)
+    )
+
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+        response => {
+            return response
+        },
+        async (error) => {
+            const prevRequest = error?.config
+            if (error?.response?.status === 401 && !prevRequest?.sent) {
+                prevRequest.sent = true
+
+                dispatch(refreshToken())
+
+                prevRequest.headers['token'] = `Bearer ${user.token}`
+
+                return axiosPrivate(prevRequest)
+            }
+
+            return Promise.reject(error)
         }
-    }, [user])
+    )
+
+    // return () => {
+    //     axiosPrivate.interceptors.request.eject(requestInterCept)
+    //     axiosPrivate.interceptors.response.eject(responseIntercept)
+    // }
+    // }, [user])
 
     return axiosPrivate
 }
